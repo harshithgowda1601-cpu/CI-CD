aab6650b97fb566fa451a11692d1aee64844b15c
Harshith <harshithgowda1601@gmail.com>
2026-09-16 02:27:24 +0000

    test jenkins webhooks

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("CI/CD Pipeline Application");
+        System.out.println("Hello from jenkins cicd");
     }
 }
